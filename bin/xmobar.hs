4832539e6cde9dc07e8e2a4f9d6add6deb7028c0
ndwarshuis <ndwar@yavin4.ch>
2020-03-15 13:12:01 -0400

    ADD xmobar config

---
+++ bin/xmobar.hs
@@ -0,0 +1,100 @@
+import Xmobar
+
+fgColor0 = "black"
+fgColor1 = "#888888"
+bgColor0 = "#eeeeee"
+bdColor = "#cccccc"
+
+wrapColor c s = "<fc=" ++ c ++ ">" ++ s ++ "</fc>"
+
+sep = wrapColor fgColor1 " : "
+
+myTemplate = concat
+  [ "%UnsafeStdinReader%"
+  , " }{ "
+  , "%wlp0s20f3wi%"
+  , sep, "%alsa:default:Master%"
+  , sep, "%battery%"
+  , sep, "%bright%"
+  , sep, "%locks%"
+  , sep, "%date% "
+  ] 
+
+config = defaultConfig { 
+  font = "xft:DejaVu Sans Mono:size=11:bold:antialias=true"
+  , additionalFonts =
+    [ "xft:FontAwesome:pixelsize=13:antialias=true:hinting=true"
+    , "xft:Symbola:size=13:bold:antialias=true"
+    ]
+  , textOffset = 16
+  , textOffsets = [ 16, 17 ]
+  , bgColor = bgColor0
+  , fgColor = fgColor0
+  , position = BottomSize C 100 24
+  , border = NoBorder
+  , borderColor = bdColor
+
+  , sepChar = "%"
+  , alignSep = "}{"
+  , template = myTemplate
+
+  , lowerOnStart = False
+  , hideOnStart = False
+  , allDesktops = True
+  , overrideRedirect = True
+  , pickBroadest = False
+  , persistent = True
+  , iconRoot = "/home/ndwar/.local/share/xmobar/icons"
+
+  , commands = 
+      [ Run $ Alsa "default" "Master"
+        [ "-t", "<status><volume>%"
+        , "--"
+        , "-O", "<fn=1>\xf028</fn>"
+        , "-o", "<fn=1>\xf026 </fn>"
+        , "-c", fgColor0
+        , "-C", fgColor0
+        ]
+
+      , Run $ Battery [ "--template", "<acstatus><left>"
+                    , "--Low", "10"
+                    , "--High", "80"
+                    , "--low", "red"
+                    , "--normal", fgColor0
+                    , "--high", fgColor0
+                    , "--"
+                    , "-P"
+                    , "-o" , "<fn=1>\xf0e7</fn>"
+                    , "-O" , "<fn=1>\xf1e6</fn>"
+                    , "-i" , "<fn=1>\xf1e6</fn>"
+                    ] 50
+
+      , Run $ Brightness ["-t", "<fn=1>\xf185</fn><percent>%"
+                       , "--"
+                       , "-D", "intel_backlight"
+                       ] 10
+
+      , Run $ Wireless "wlp0s20f3"
+        [ "-t", "<qualityipat><essid>"
+        , "--"
+        , "--quality-icon-pattern", "<icon=wifi_%%.xpm/>"
+        ] 5
+
+      , Run $ Locks
+        [ "-N", "<fn=2>\x1f13d</fn>"
+        , "-n", wrapColor fgColor1 "<fn=2>\x1f13d</fn>"
+        , "-C", "<fn=2>\x1f132</fn>"
+        , "-c", wrapColor fgColor1 "<fn=2>\x1f132</fn>"
+        , "-s", ""
+        , "-S", ""
+        , "-d", "<fn=2> </fn>"
+        ]
+
+      , Run $ Date "%Y-%m-%d %H:%M" "date" 10
+
+      , Run UnsafeStdinReader
+      ]
+  }
+
+main :: IO ()
+main = xmobar config
